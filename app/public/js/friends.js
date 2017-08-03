83ae37f59e057f62e390a090e29e0a31de94fc66
Don Bedwell <timetourst@gmail.com>
2017-08-02 22:53:48 -0400

    11:00 PM push

--- app/public/js/friends.js
+++ app/public/js/friends.js
@@ -1,21 +1,18 @@
-function runTableQuery(){
-
-	// Here we get the location of the root page.
-	// We use this instead of explicitly saying the URL is localhost:3001 because the url will change when we deploy.
+function goMakeFriends(){
+	// root page
 	var currentURL = window.location.origin;
 
-	// The AJAX function uses the URL of our API to GET the data associated with it (initially set to localhost)
+	// AJAX function uses the API URL to GET data associated with it
 	$.ajax({url: currentURL + "/api/friends", method: "GET"})
 		.done(function(friendData) {
-
 			// Here we are logging the URL so we have access to it for troubleshooting
-			console.log("------------------------------------");
-			console.log("URL: " + currentURL + "/api/friends");
-			console.log("------------------------------------");
+			//console.log("------------------------------------");
+			//console.log("URL: " + currentURL + "/api/friends");
+			//console.log("------------------------------------");
 
 			// Here we then log the NYTData to console, where it will show up as an object.
-			console.log(friendData);
-			console.log("------------------------------------")
+			//console.log(friendData);
+			//console.log("------------------------------------")
 
 			// Loop through and display each of the customers
 			for (var i = 0; i < friendData.length; i++){
@@ -26,8 +23,6 @@ function runTableQuery(){
 			friendsSection.attr('id', 'friendsWell-' + i+1)
 			$('#friendsSection').append(friendsSection);
 
-			//var tableNumber = i + 1;
-
 
 			// Then display the remaining fields in the HTML (Section Name, Date, URL)
 			$("#friendsWell-"+ i+1).append('<h2><span class="label label-primary">' + tableNumber + "</span> | " + tableData[i].customerID + "</h2>");
@@ -35,4 +30,4 @@ function runTableQuery(){
 	});
 }
 
-runTableQuery();
+goMakeFriends();
--- server.js
+++ server.js
@@ -16,34 +16,92 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // set absolute pathing
 app.use(express.static(__dirname + '/app/public'));
 
-// set array of friends
-var friends = [];
+// set array of friends. Information from survey form is appended
+// with default data added
+var friends = [
+	{
+		"name": "Don Bedwell",
+		"photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq2AAAAJGZhMWQwOTY3LTAxZmQtNDg0Ny1hZjBjLTgyNjk2YWU0YzQxOA.jpg",
+		"scores": [
+			"5",
+			"5",
+			"3",
+			"1",
+			"3",
+			"4",
+			"5",
+			"1",
+			"3",
+			"2"
+			]
+	},
+	{
+		"name": "Jeremy Johansen",
+		"photo": "http://i.huffpost.com/gadgets/slideshows/10591/slide_10591_139838_large.jpg",
+		"scores": [
+			"4",
+			"5",
+			"1",
+			"1",
+			"1",
+			"4",
+			"3",
+			"5",
+			"2",
+			"4"
+		]
+	},
+	{
+		"name": "Scooter Petersen",
+		"photo": "http://www.teamjimmyjoe.com/wp-content/uploads/2015/09/awkward-family-photos-portrait-hawaiian-shirt.jpg",
+		"scores": [
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5"
+		]
+	},
+	{
+		"name": "Mandy Ralston",
+		"photo": "https://s-media-cache-ak0.pinimg.com/736x/cf/e7/e2/cfe7e2333d0f93b30d8e14ec8f98b502--family-pics-awkward-family-photos.jpg",
+		"scores": [
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3"
+		]
+	}
+];
 
 // set routes
+// index (home) page
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "app/public/home.html"));
 });
-
+// survey page
 app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname, "app/public/survey.html"));
 });
-
-// get API friends list
+// get API friends list (the raw data)
 app.get("/api/friends", function(req, res) {
   res.json(friends);
 });
-
-// post to send data to the object array
+// post data to the object array
 app.post("/api/friends", function(req, res) {
-
-	//if(tables.length < 5){
-  		friends.push(req.body);
-  		//res.send(true);
-  	//} else {
-  	//	waitList.push(req.body);
-  	//	res.send(false);
-  	//}
-  res.json(friends);
+	friends.push(req.body);
+	res.json(friends);
 });
 
 // start server listener
